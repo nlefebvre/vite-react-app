9aae014c69c71d152fc92e8b335ecc6fc45975c6
Nicolas Lefebvre <nicolas.j.lefebvre@gmail.com>
2024-12-19 10:45:33 -0700

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -6,26 +6,34 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { BrowserRouter, Routes, Route } from "react-router";
+// import ConnectionsApp from "./connections/ConnectionsApp.tsx";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator formFields={{
-      signUp: {
-        displayName: {
-          label: 'Display Name',
-          isRequired: true,
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/connections" element={<ConnectionsApp />} /> */}
+        <Route path="/" element={<Authenticator formFields={{
+          signUp: {
+            displayName: {
+              label: 'Display Name',
+              isRequired: true,
 
-        }
-      }
-    }}>
-      {({ signOut, user, }) => (
-        <main>
-          <button onClick={signOut}>Sign out</button>
-          <App />
-        </main>
-      )}
-    </Authenticator>
+            }
+          }
+        }}>
+          {({ signOut, user, }) => (
+            <main>
+              <button onClick={signOut}>Sign out</button>
+              <App />
+            </main>
+          )}
+        </Authenticator>}
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
